61dc2c7186d19a0901713b41dad615921da41f57
kiran0702 <kiran7722p@gmail.com>
2025-08-05 20:52:51 +0530

    loader

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -17,7 +17,7 @@ import toast from "react-hot-toast";
 import ChatLoader from '../components/ChatLoader';
 import CallButton from '../components/CallButton';
 import { Check } from 'lucide-react';
-const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY
 
 const ChatPage = () => {
   const { id: targetUserId } = useParams();
